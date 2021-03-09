dae00ce97a065a1fc2fe79e3c99cd118d12f737c
Oosjoost <joostensandy@gmail.com>
2021-03-09 16:41:52 +0100

    missing bluswater_defect in alternatieve

--- migratiescripts/migratie_3.3.0-3.3.1.sql
+++ migratiescripts/migratie_3.3.0-3.3.1.sql
@@ -4,6 +4,9 @@ SET search_path = objecten, pg_catalog, public;
 UPDATE objecten.veiligh_ruimtelijk_type SET symbol_name = 'reduceer_drukbegrenzer' WHERE symbol_name = 'reduceer_drukbegerenzer';
 UPDATE dreiging_type SET symbol_name = 'algemeen_gevaar' WHERE id = 129;
 
+INSERT INTO bluswater.alternatieve_type (id, naam, symbol_name, size)
+    VALUES (9999, 'Voorziening defect', 'bluswater_defect', 6) ON CONFLICT DO NOTHING;
+
 -- Update versie van de applicatie
 UPDATE algemeen.applicatie SET sub = 3;
 UPDATE algemeen.applicatie SET revisie = 0;
