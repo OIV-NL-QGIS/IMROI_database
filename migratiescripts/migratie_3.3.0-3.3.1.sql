2111f0dd5ab5938f06af37c8b27b55d7ccb2c8d9
Oosjoost <joostensandy@gmail.com>
2021-03-09 16:29:44 +0100

    bug fix algemeen_gevaar symbool naam

--- migratiescripts/migratie_3.3.0-3.3.1.sql
+++ migratiescripts/migratie_3.3.0-3.3.1.sql
@@ -2,6 +2,7 @@ SET ROLE oiv_admin;
 SET search_path = objecten, pg_catalog, public;
 
 UPDATE objecten.veiligh_ruimtelijk_type SET symbol_name = 'reduceer_drukbegrenzer' WHERE symbol_name = 'reduceer_drukbegerenzer';
+UPDATE dreiging_type SET symbol_name = 'algemeen_gevaar' WHERE id = 129;
 
 -- Update versie van de applicatie
 UPDATE algemeen.applicatie SET sub = 3;
