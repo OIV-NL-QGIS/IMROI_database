c65590e9f372f6b67263e27940dce84967def5c2
Oosjoost <joostensandy@gmail.com>
2021-03-16 12:04:50 +0100

    Update migratie_3.3.0-3.3.1.sql

--- migratiescripts/migratie_3.3.0-3.3.1.sql
+++ migratiescripts/migratie_3.3.0-3.3.1.sql
@@ -18,6 +18,6 @@ INSERT INTO algemeen.styles(laagnaam, soortnaam, lijndikte, lijnkleur, lijnstijl
 
 -- Update versie van de applicatie
 UPDATE algemeen.applicatie SET sub = 3;
-UPDATE algemeen.applicatie SET revisie = 0;
-UPDATE algemeen.applicatie SET db_versie = 330; -- db versie == versie_sub_revisie
+UPDATE algemeen.applicatie SET revisie = 1;
+UPDATE algemeen.applicatie SET db_versie = 331; -- db versie == versie_sub_revisie
 UPDATE algemeen.applicatie SET datum = now();
