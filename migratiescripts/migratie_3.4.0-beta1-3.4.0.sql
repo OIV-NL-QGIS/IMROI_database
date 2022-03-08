91ab3e9f1a0e3bea121a3eb1e0785621d9f20a84
Oosjoost <joostensandy@gmail.com>
2022-03-08 09:15:57 +0100

    Update migratie_3.4.0-beta1-3.4.0.sql

--- migratiescripts/migratie_3.4.0-beta1-3.4.0.sql
+++ migratiescripts/migratie_3.4.0-beta1-3.4.0.sql
@@ -29,6 +29,36 @@ CREATE RULE bouwlagen_ins AS
   VALUES (new.geom, new.bouwlaag, new.bouwdeel, new.pand_id, new.fotografie_id)
   RETURNING *;
 
+DROP RULE IF EXISTS object_bgt_ins ON objecten.object_bgt;
+CREATE RULE object_bgt_ins AS
+    ON INSERT TO objecten.object_bgt DO INSTEAD  INSERT INTO objecten.object (geom, basisreg_identifier, formelenaam, bron, bron_tabel)  SELECT new.geom,
+            b.identificatie,
+            new.formelenaam,
+            b.bron,
+            b.bron_tbl
+           FROM algemeen.bgt_extent b
+          WHERE st_intersects(new.geom, st_curvetoline(b.geovlak))
+         LIMIT 1
+  RETURNING object.id,
+    object.geom,
+    object.datum_aangemaakt,
+    object.datum_gewijzigd,
+    object.basisreg_identifier,
+    object.formelenaam,
+    object.bijzonderheden,
+    object.pers_max,
+    object.pers_nietz_max,
+    object.datum_geldig_tot,
+    object.datum_geldig_vanaf,
+    object.bron,
+    object.bron_tabel,
+    object.fotografie_id,
+    object.bodemgesteldheid_type_id,
+    ( SELECT o.typeobject
+           FROM objecten.historie o
+          WHERE (o.object_id = object.id)
+         LIMIT 1) AS typeobject;
+
 -- Update versie van de applicatie
 UPDATE algemeen.applicatie SET sub = 4;
 UPDATE algemeen.applicatie SET revisie = 0;
