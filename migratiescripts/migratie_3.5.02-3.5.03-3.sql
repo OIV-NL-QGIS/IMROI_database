566e03d8c27e5a25ee3a8c3f982de23628b78572
Oostjoost <joostensandy@gmail.com>
2024-02-14 14:32:01 +0100

    Update migratie_3.5.02-3.5.03-3.sql
    update to objecten views

--- migratiescripts/migratie_3.5.02-3.5.03-3.sql
+++ migratiescripts/migratie_3.5.02-3.5.03-3.sql
@@ -316,8 +316,462 @@ AS SELECT v.id,
      JOIN objecten.veiligh_install_type vt ON v.veiligh_install_type_id = vt.id
   WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
 
+CREATE OR REPLACE VIEW objecten.object_bereikbaarheid
+AS SELECT l.id,
+    l.geom,
+    l.soort,
+    l.label,
+    l.obstakels,
+    l.wegafzetting,
+    l.fotografie_id,
+    l.object_id,
+    b.formelenaam,
+    ''::text AS applicatie,
+    part.typeobject
+   FROM objecten.bereikbaarheid l
+     JOIN objecten.object b ON l.object_id = b.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE l.parent_deleted = 'infinity'::timestamp with time zone AND l.self_deleted = 'infinity'::timestamp with time zone;
 
+CREATE OR REPLACE VIEW objecten.object_bgt
+AS SELECT b.id,
+    b.geom,
+    b.datum_aangemaakt,
+    b.datum_gewijzigd,
+    b.basisreg_identifier,
+    b.formelenaam,
+    b.bijzonderheden,
+    b.pers_max,
+    b.pers_nietz_max,
+    b.datum_geldig_tot,
+    b.datum_geldig_vanaf,
+    b.bron,
+    b.bron_tabel,
+    b.fotografie_id,
+    b.bodemgesteldheid_type_id,
+    part.typeobject
+   FROM objecten.object b
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE b.bron::text = 'BGT'::text AND b.self_deleted = 'infinity'::timestamp with time zone;
 
+CREATE OR REPLACE VIEW objecten.object_dreiging
+AS SELECT v.id,
+    v.geom,
+    v.dreiging_type_id,
+    v.label,
+    v.fotografie_id,
+    v.omschrijving,
+    v.bouwlaag_id,
+    v.object_id,
+    b.formelenaam,
+    v.rotatie,
+    st.symbol_name,
+    st.size_object AS size,
+    ''::text AS applicatie,
+    part.typeobject,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot
+   FROM objecten.dreiging v
+     JOIN objecten.object b ON v.object_id = b.id
+     JOIN objecten.dreiging_type st ON v.dreiging_type_id = st.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_gebiedsgerichte_aanpak
+AS SELECT l.id,
+    l.geom,
+    l.soort,
+    l.label,
+    l.bijzonderheden,
+    l.fotografie_id,
+    l.object_id,
+    b.formelenaam,
+    ''::text AS applicatie,
+    part.typeobject,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot
+   FROM objecten.gebiedsgerichte_aanpak l
+     JOIN objecten.object b ON l.object_id = b.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE l.parent_deleted = 'infinity'::timestamp with time zone AND l.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_grid
+AS SELECT b.id,
+    b.geom,
+    b.datum_aangemaakt,
+    b.datum_gewijzigd,
+    b.y_as_label,
+    b.x_as_label,
+    b.object_id,
+    b.afstand,
+    b.vaknummer,
+    b.scale,
+    b.papersize,
+    b.orientation,
+    b.type,
+    b.uuid,
+    o.formelenaam,
+    o.datum_geldig_vanaf,
+    o.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.grid b
+     JOIN objecten.object o ON b.object_id = o.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON o.id = part.object_id
+  WHERE b.parent_deleted = 'infinity'::timestamp with time zone AND b.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_ingang
+AS SELECT v.id,
+    v.geom,
+    v.ingang_type_id,
+    v.label,
+    v.belemmering,
+    v.voorzieningen,
+    v.fotografie_id,
+    v.bouwlaag_id,
+    v.object_id,
+    b.formelenaam,
+    v.rotatie,
+    it.symbol_name,
+    it.size_object AS size,
+    ''::text AS applicatie,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.ingang v
+     JOIN objecten.object b ON v.object_id = b.id
+     JOIN objecten.ingang_type it ON v.ingang_type_id = it.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_isolijnen
+AS SELECT l.id,
+    l.geom,
+    l.hoogte,
+    l.omschrijving,
+    l.object_id,
+    b.formelenaam,
+    ''::text AS applicatie,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.isolijnen l
+     LEFT JOIN objecten.object b ON l.object_id = b.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE l.parent_deleted = 'infinity'::timestamp with time zone AND l.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_label
+AS SELECT l.id,
+    l.geom,
+    l.soort,
+    l.omschrijving,
+    l.bouwlaag_id,
+    l.object_id,
+    b.formelenaam,
+    l.rotatie,
+    st.symbol_name,
+    st.size_object AS size,
+    ''::text AS applicatie,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.label l
+     JOIN objecten.object b ON l.object_id = b.id
+     JOIN objecten.label_type st ON l.soort::text = st.naam::text
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE l.parent_deleted = 'infinity'::timestamp with time zone AND l.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_objecten
+AS SELECT DISTINCT b.id,
+    b.geom,
+    b.datum_aangemaakt,
+    b.datum_gewijzigd,
+    b.basisreg_identifier,
+    b.formelenaam,
+    b.bijzonderheden,
+    b.pers_max,
+    b.pers_nietz_max,
+    b.datum_geldig_tot,
+    b.datum_geldig_vanaf,
+    b.bron,
+    b.bron_tabel,
+    b.fotografie_id,
+    b.bodemgesteldheid_type_id,
+    b.min_bouwlaag,
+    b.max_bouwlaag,
+    part.typeobject
+   FROM objecten.object b
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE b.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_opslag
+AS SELECT o.id,
+    o.geom,
+    o.datum_aangemaakt,
+    o.datum_gewijzigd,
+    o.locatie,
+    o.bouwlaag_id,
+    o.object_id,
+    o.fotografie_id,
+    b.formelenaam,
+    o.rotatie,
+    st.size_object AS size,
+    st.symbol_name,
+    ''::text AS applicatie,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.gevaarlijkestof_opslag o
+     JOIN objecten.object b ON o.object_id = b.id
+     JOIN objecten.gevaarlijkestof_opslag_type st ON 'Opslag stoffen'::text = st.naam
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE o.parent_deleted = 'infinity'::timestamp with time zone AND o.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_opstelplaats
+AS SELECT l.id,
+    l.geom,
+    l.soort,
+    l.label,
+    l.fotografie_id,
+    l.object_id,
+    b.formelenaam,
+    l.rotatie,
+    st.symbol_name,
+    st.size,
+    ''::text AS applicatie,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.opstelplaats l
+     JOIN objecten.object b ON l.object_id = b.id
+     JOIN objecten.opstelplaats_type st ON l.soort::text = st.naam::text
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE l.parent_deleted = 'infinity'::timestamp with time zone AND l.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_points_of_interest
+AS SELECT b.id,
+    b.geom,
+    b.points_of_interest_type_id,
+    b.label,
+    b.bijzonderheid,
+    b.fotografie_id,
+    b.object_id,
+    o.formelenaam,
+    b.rotatie,
+    vt.symbol_name,
+    vt.size,
+    ''::text AS applicatie,
+    o.datum_geldig_vanaf,
+    o.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.points_of_interest b
+     JOIN objecten.object o ON b.object_id = o.id
+     JOIN objecten.points_of_interest_type vt ON b.points_of_interest_type_id = vt.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON o.id = part.object_id
+  WHERE b.parent_deleted = 'infinity'::timestamp with time zone AND b.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_scenario_locatie
+AS SELECT o.id,
+    o.geom,
+    o.datum_aangemaakt,
+    o.datum_gewijzigd,
+    o.locatie,
+    o.bouwlaag_id,
+    o.object_id,
+    o.fotografie_id,
+    o.rotatie,
+    st.size_object AS size,
+    st.symbol_name,
+    ''::text AS applicatie,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.scenario_locatie o
+     JOIN objecten.object b ON o.object_id = b.id
+     JOIN objecten.scenario_locatie_type st ON 'Scenario locatie'::text = st.naam
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE o.parent_deleted = 'infinity'::timestamp with time zone AND o.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_sectoren
+AS SELECT l.id,
+    l.geom,
+    l.soort,
+    l.label,
+    l.omschrijving,
+    l.fotografie_id,
+    l.object_id,
+    b.formelenaam,
+    ''::text AS applicatie,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.sectoren l
+     JOIN objecten.object b ON l.object_id = b.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE l.parent_deleted = 'infinity'::timestamp with time zone AND l.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_sleutelkluis
+AS SELECT v.id,
+    v.geom,
+    v.sleutelkluis_type_id,
+    v.label,
+    v.aanduiding_locatie,
+    v.sleuteldoel_type_id,
+    v.fotografie_id,
+    v.ingang_id,
+    b.formelenaam,
+    v.rotatie,
+    it.symbol_name,
+    it.size_object AS size,
+    ''::text AS applicatie,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.sleutelkluis v
+     JOIN objecten.ingang i ON v.ingang_id = i.id
+     JOIN objecten.sleutelkluis_type it ON v.sleutelkluis_type_id = it.id
+     JOIN objecten.object b ON i.object_id = b.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_terrein
+AS SELECT b.id,
+    b.geom,
+    b.datum_aangemaakt,
+    b.datum_gewijzigd,
+    b.omschrijving,
+    b.object_id,
+    o.formelenaam,
+    o.datum_geldig_vanaf,
+    o.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.terrein b
+     JOIN objecten.object o ON b.object_id = o.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON o.id = part.object_id
+  WHERE b.parent_deleted = 'infinity'::timestamp with time zone AND b.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.object_veiligh_ruimtelijk
+AS SELECT b.id,
+    b.geom,
+    b.veiligh_ruimtelijk_type_id,
+    b.label,
+    b.bijzonderheid,
+    b.fotografie_id,
+    b.object_id,
+    o.formelenaam,
+    b.rotatie,
+    vt.symbol_name,
+    vt.size,
+    ''::text AS applicatie,
+    o.datum_geldig_vanaf,
+    o.datum_geldig_tot,
+    part.typeobject
+   FROM objecten.veiligh_ruimtelijk b
+     JOIN objecten.object o ON b.object_id = o.id
+     JOIN objecten.veiligh_ruimtelijk_type vt ON b.veiligh_ruimtelijk_type_id = vt.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON o.id = part.object_id
+  WHERE b.parent_deleted = 'infinity'::timestamp with time zone AND b.self_deleted = 'infinity'::timestamp with time zone;
 
 
 
