b5d297bd243fffc1dd711e4e5b063c1aba49cd20
Oostjoost <joostensandy@gmail.com>
2024-02-14 13:50:18 +0100

    Update migratie_3.5.03-3.5.04.sql
    update to bouwlagen views

--- migratiescripts/migratie_3.5.03-3.5.04.sql
+++ migratiescripts/migratie_3.5.03-3.5.04.sql
@@ -125,8 +125,196 @@ ALTER TABLE objecten.veilighv_org RENAME COLUMN datum_deleted TO parent_deleted;
 ALTER TABLE objecten.veilighv_org ADD COLUMN self_deleted timestamptz default 'infinity';
 CREATE TRIGGER trg_set_delete_after_parent AFTER UPDATE OF parent_deleted ON objecten.veilighv_org FOR EACH ROW EXECUTE FUNCTION objecten.set_delete_timestamp();
 
+CREATE OR REPLACE VIEW objecten.bouwlaag_afw_binnendekking
+AS SELECT v.id,
+    v.geom,
+    v.soort,
+    v.label,
+    v.handelingsaanwijzing,
+    v.bouwlaag_id,
+    b.bouwlaag,
+    v.rotatie,
+    st.symbol_name,
+    st.size,
+    ''::text AS applicatie
+   FROM objecten.afw_binnendekking v
+     JOIN objecten.bouwlagen b ON v.bouwlaag_id = b.id
+     JOIN objecten.afw_binnendekking_type st ON v.soort::text = st.naam::text
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
 
+CREATE OR REPLACE VIEW objecten.bouwlaag_bouwlagen
+AS SELECT b.id,
+    b.geom,
+    b.datum_aangemaakt,
+    b.datum_gewijzigd,
+    b.bouwlaag,
+    b.bouwdeel,
+    b.pand_id,
+    b.self_deleted AS datum_deleted
+   FROM objecten.bouwlagen b
+  WHERE b.self_deleted = 'infinity'::timestamp with time zone;
 
+CREATE OR REPLACE VIEW objecten.bouwlaag_dreiging
+AS SELECT v.id,
+    v.geom,
+    v.dreiging_type_id,
+    v.label,
+    v.fotografie_id,
+    v.omschrijving,
+    v.bouwlaag_id,
+    v.object_id,
+    b.bouwlaag,
+    v.rotatie,
+    st.symbol_name,
+    st.size,
+    ''::text AS applicatie
+   FROM objecten.dreiging v
+     JOIN objecten.bouwlagen b ON v.bouwlaag_id = b.id
+     JOIN objecten.dreiging_type st ON v.dreiging_type_id = st.id
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.bouwlaag_ingang
+AS SELECT v.id,
+    v.geom,
+    v.ingang_type_id,
+    v.label,
+    v.belemmering,
+    v.voorzieningen,
+    v.fotografie_id,
+    v.bouwlaag_id,
+    v.object_id,
+    b.bouwlaag,
+    v.rotatie,
+    it.symbol_name,
+    it.size,
+    ''::text AS applicatie
+   FROM objecten.ingang v
+     JOIN objecten.bouwlagen b ON v.bouwlaag_id = b.id
+     JOIN objecten.ingang_type it ON v.ingang_type_id = it.id
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.bouwlaag_label
+AS SELECT v.id,
+    v.geom,
+    v.soort,
+    v.omschrijving,
+    v.bouwlaag_id,
+    v.object_id,
+    b.bouwlaag,
+    v.rotatie,
+    st.symbol_name,
+    st.size,
+    ''::text AS applicatie
+   FROM objecten.label v
+     JOIN objecten.bouwlagen b ON v.bouwlaag_id = b.id
+     JOIN objecten.label_type st ON v.soort::text = st.naam::text
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.bouwlaag_opslag
+AS SELECT v.id,
+    v.geom,
+    v.datum_aangemaakt,
+    v.datum_gewijzigd,
+    v.locatie,
+    v.bouwlaag_id,
+    v.object_id,
+    v.fotografie_id,
+    v.rotatie,
+    b.bouwlaag,
+    st.symbol_name,
+    st.size,
+    ''::text AS applicatie
+   FROM objecten.gevaarlijkestof_opslag v
+     JOIN objecten.bouwlagen b ON v.bouwlaag_id = b.id
+     JOIN objecten.gevaarlijkestof_opslag_type st ON 'Opslag stoffen'::text = st.naam
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.bouwlaag_ruimten
+AS SELECT v.id,
+    v.geom,
+    v.ruimten_type_id,
+    v.omschrijving,
+    v.fotografie_id,
+    v.bouwlaag_id,
+    b.bouwlaag,
+    ''::text AS applicatie
+   FROM objecten.ruimten v
+     JOIN objecten.bouwlagen b ON v.bouwlaag_id = b.id
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.bouwlaag_scenario_locatie
+AS SELECT v.id,
+    v.geom,
+    v.datum_aangemaakt,
+    v.datum_gewijzigd,
+    v.locatie,
+    v.bouwlaag_id,
+    v.object_id,
+    v.fotografie_id,
+    v.rotatie,
+    b.bouwlaag,
+    st.symbol_name,
+    st.size,
+    ''::text AS applicatie
+   FROM objecten.scenario_locatie v
+     JOIN objecten.bouwlagen b ON v.bouwlaag_id = b.id
+     JOIN objecten.scenario_locatie_type st ON 'Scenario locatie'::text = st.naam
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.bouwlaag_sleutelkluis
+AS SELECT v.id,
+    v.geom,
+    v.sleutelkluis_type_id,
+    v.label,
+    v.aanduiding_locatie,
+    v.sleuteldoel_type_id,
+    v.fotografie_id,
+    v.ingang_id,
+    part.bouwlaag,
+    v.rotatie,
+    it.symbol_name,
+    it.size,
+    ''::text AS applicatie
+   FROM objecten.sleutelkluis v
+     JOIN ( SELECT b.bouwlaag,
+            ib.id,
+            ib.bouwlaag_id
+           FROM objecten.ingang ib
+             JOIN objecten.bouwlagen b ON ib.bouwlaag_id = b.id) part ON v.ingang_id = part.id
+     JOIN objecten.sleutelkluis_type it ON v.sleutelkluis_type_id = it.id
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.bouwlaag_veiligh_bouwk
+AS SELECT v.id,
+    v.geom,
+    v.soort,
+    v.fotografie_id,
+    v.bouwlaag_id,
+    b.bouwlaag,
+    ''::text AS applicatie
+   FROM objecten.veiligh_bouwk v
+     JOIN objecten.bouwlagen b ON v.bouwlaag_id = b.id
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
+
+CREATE OR REPLACE VIEW objecten.bouwlaag_veiligh_install
+AS SELECT v.id,
+    v.geom,
+    v.datum_aangemaakt,
+    v.datum_gewijzigd,
+    v.veiligh_install_type_id,
+    v.label,
+    v.bijzonderheid,
+    v.bouwlaag_id,
+    v.rotatie,
+    v.fotografie_id,
+    b.bouwlaag,
+    vt.size,
+    vt.symbol_name,
+    ''::text AS applicatie
+   FROM objecten.veiligh_install v
+     JOIN objecten.bouwlagen b ON v.bouwlaag_id = b.id
+     JOIN objecten.veiligh_install_type vt ON v.veiligh_install_type_id = vt.id
+  WHERE v.parent_deleted = 'infinity'::timestamp with time zone AND v.self_deleted = 'infinity'::timestamp with time zone;
 
 
 
