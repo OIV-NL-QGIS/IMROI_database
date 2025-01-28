b9402bbf77ba3f00cba3067c01b76b79fecee615
Oosjoost <joostensandy@gmail.com>
2025-01-28 12:05:09 +0100

    Update bag_extent_per_regio_new.sql

--- bag_extent_per_regio_new.sql
+++ bag_extent_per_regio_new.sql
@@ -9,7 +9,7 @@ BEGIN
 	--Vervang 'search_path = latest' door 'search_path = bag_schema_naam', waar bag_schema_naam de schemanaam is is jouw database
 	SET search_path = latest, pg_catalog, public;
 
-	DROP TABLE IF EXISTS bgt.bag_extent ;
+	DROP TABLE IF EXISTS algemeen.bag_extent_new;
 	CREATE TABLE bgt.bag_extent
 	( 
 		gid 			serial primary key,
@@ -21,7 +21,7 @@ BEGIN
 		bron_tbl 		character varying(254)
 	);
 
-	INSERT INTO bgt.bag_extent (identificatie, status, geovlak, gebruiksdoel, bron, bron_tbl)
+	INSERT INTO algemeen.bag_extent_new (identificatie, status, geovlak, gebruiksdoel, bron, bron_tbl)
 	SELECT p.identificatie, p.pandstatus, ST_Multi(ST_Force2D(p.geovlak))::geometry(MultiPolygon, 28992), gebr.gebruiksdoel, 'BAG', 'Pand' FROM algemeen.veiligheidsregio_watergrens v
 		LEFT JOIN bagactueel.pandactueel p ON ST_INTERSECTS(p.geovlak, v.geom)
 		LEFT JOIN (
@@ -35,22 +35,12 @@ BEGIN
 			) gebr ON p.identificatie = gebr.gerelateerdpand
 	WHERE v.code = regio;
 
-	INSERT INTO bgt.bag_extent (identificatie, status, geovlak, gebruiksdoel, bron, bron_tbl)
-	SELECT o.gml_id, o."bgt-status", ST_Multi(ST_CurveToLine(o.geom))::geometry(MultiPolygon,28992), 'overigbouwwerk', 'BGT', 'Overigbouwwerk' FROM algemeen.veiligheidsregio_watergrens v
-		LEFT JOIN bgt.overigbouwwerk o ON ST_3DINTERSECTS(v.geom, o.geom)
-	WHERE v.code = '12';
-
-	INSERT INTO bgt.bag_extent (identificatie, status, geovlak, gebruiksdoel, bron, bron_tbl)
-	SELECT o.gml_id, o."bgt-status", ST_Multi(ST_CurveToLine(o.geom))::geometry(MultiPolygon,28992), 'tunneldeel', 'BGT', 'Tunneldeel' FROM algemeen.veiligheidsregio_watergrens v
-		LEFT JOIN bgt.tunneldeel o ON ST_3DINTERSECTS(v.geom, o.geom)
-	WHERE v.code = '12';
-
-	INSERT INTO algemeen.bag_extent (identificatie, status, geovlak, gebruiksdoel, bron, bron_tbl)
+	INSERT INTO algemeen.bag_extent_new (identificatie, status, geovlak, gebruiksdoel, bron, bron_tbl)
 	SELECT l.identificatie, l.ligplaatsstatus::TEXT, ST_Multi(ST_Force2D(l.geovlak))::geometry(MultiPolygon, 28992), 'ligplaats', 'BAG', 'Ligplaats' FROM algemeen.veiligheidsregio_watergrens v 
 		LEFT JOIN bagactueel.ligplaatsactueel l ON ST_INTERSECTS(v.geom, l.geovlak)
 	WHERE v.code = regio;
 
-	INSERT INTO algemeen.bag_extent (identificatie, status, geovlak, gebruiksdoel, bron, bron_tbl)
+	INSERT INTO algemeen.bag_extent_new (identificatie, status, geovlak, gebruiksdoel, bron, bron_tbl)
 	SELECT l.identificatie, l.standplaatsstatus::TEXT, ST_Multi(ST_Force2D(l.geovlak))::geometry(MultiPolygon, 28992), 'standplaats', 'BAG', 'Standplaats' FROM algemeen.veiligheidsregio_watergrens v 
 		LEFT JOIN bagactueel.standplaatsactueel l ON ST_INTERSECTS(v.geom, l.geovlak)
 	WHERE v.code = regio;
