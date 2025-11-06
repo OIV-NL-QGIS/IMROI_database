3118d9e03f0bf48f8ee94c4a42fe8450d4d8cee9
Oostjoost <joostensandy@gmail.com>
2025-11-06 12:52:53 +0100

    Create migratie_3.6.12-3.6.13.sql

---
+++ migratiescripts/migratie_3.6.12-3.6.13.sql
@@ -0,0 +1,34 @@
+SET ROLE oiv_admin;
+
+CREATE OR REPLACE VIEW objecten.object_bereikbaarheid
+AS SELECT l.id,
+    l.geom,
+    l.soort,
+    l.label,
+    l.opmerking,
+    l.fotografie_id,
+    l.object_id,
+    b.formelenaam,
+    ''::text AS applicatie,
+    part.typeobject,
+    b.datum_geldig_vanaf,
+    b.datum_geldig_tot
+   FROM objecten.bereikbaarheid l
+     JOIN objecten.object b ON l.object_id = b.id
+     LEFT JOIN ( SELECT h.object_id,
+            h.typeobject
+           FROM objecten.historie h
+             JOIN ( SELECT historie.object_id,
+                    max(historie.datum_aangemaakt) AS maxdatetime
+                   FROM objecten.historie
+                  GROUP BY historie.object_id) hist ON h.object_id = hist.object_id AND h.datum_aangemaakt = hist.maxdatetime) part ON b.id = part.object_id
+  WHERE l.parent_deleted = 'infinity'::timestamp with time zone AND l.self_deleted = 'infinity'::timestamp with time zone;
+
+UPDATE objecten.object SET size = 8 WHERE size = 0;
+
+  -- Update versie van de applicatie
+UPDATE algemeen.applicatie SET sub = 6;
+UPDATE algemeen.applicatie SET revisie = 13;
+UPDATE algemeen.applicatie SET db_versie = 3613; -- db versie == versie_sub_revisie
+UPDATE algemeen.applicatie SET omschrijving = '';
+UPDATE algemeen.applicatie SET datum = now();
